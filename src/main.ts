0c3c2ede835a20e2cf2a0d813d473fd818b698e4
Lucca Costa <lucca0403@gmail.com>
2025-05-29 13:26:18 -0300

    fix: swagger on main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'node_modules', 'swagger-ui-dist'));
+
+  app.enableCors({
+    origin: [
+      'https://back-music-lib-h2eggwera6hydgb3.eastus-01.azurewebsites.net',
+      'http://localhost:3000'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
   .setTitle('Music Library Backend')
   .setDescription('Api for music library application')
   .setVersion('0.1')
-  .addTag('Preview')
-  .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'access-token')
+  .addServer('https://back-music-lib-h2eggwera6hydgb3.eastus-01.azurewebsites.net')
+  .addServer('http://localhost:3000')
+  .addBearerAuth()
   .build();
 
   app.useGlobalPipes(
@@ -27,6 +41,6 @@ async function bootstrap() {
   SwaggerModule.setup('swagger', app, document);
 
   await app.listen(process.env.PORT || 3000);
-  
+
 }
 bootstrap();
